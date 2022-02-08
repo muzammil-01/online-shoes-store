fda9acb4136c8bd0c3ed57daf31856f614a2fa90
muzammil-01 <65038682+muzammil-01@users.noreply.github.com>
2022-02-08 23:36:44 +0500

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,77 @@
+const products = [
+    {
+      
+        name:'Airpods Wireless Bluetooth Headphones',
+        image:'/images/shoe1.png',
+        description:
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock:3,
+        rating:4.5,
+        numReviews:4,
+    },
+    {
+      
+        name:'Airpods Wireless Bluetooth Headphones',
+        image:'/images/shoe1.png',
+        description:
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock:3,
+        rating:1.5,
+        numReviews:4,
+    },
+    {
+        name:'Airpods Wireless Bluetooth Headphones',
+        image:'/images/shoe2.png',
+        description:
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock:3,
+        rating:2.5,
+        numReviews:4,
+    },
+    {
+        name:'Airpods Wireless Bluetooth Headphones',
+        image:'/images/shoe3.png',
+        description:
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock:3,
+        rating:3,
+        numReviews:4,
+    },
+    {
+        name:'Airpods Wireless Bluetooth Headphones',
+        image:'/images/shoe1.png',
+        description:
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock:3,
+        rating:4,
+        numReviews:4,
+    },
+    {
+        name:'Airpods Wireless Bluetooth Headphones',
+        image:'/images/shoe1.png',
+        description:
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        brand:'Apple',
+        category:'Electronics',
+        price:89.99,
+        countInStock:3,
+        rating:5,
+        numReviews:4,
+    }
+]
+export default   products;
